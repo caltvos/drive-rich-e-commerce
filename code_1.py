a9ac5b7426f5e5fad5cc2363ddee756902046571
caltvos <152297554+caltvos@users.noreply.github.com>
2024-04-18 14:20:15 +0000

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,33 @@
+    const data = [];
+
+        console.log("Random Number: " + item);
+}
+    });
+
+    const data = generateRandomData();
+main();
+
+    return data;
+function generateRandomData() {
+    }
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+    data.forEach(item => {
+}    const data = generateRandomData();
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    for (let i = 0; i < 10; i++) {
+function main() {
+    const data = [];
+    data.forEach(item => {
+    }
+}
+}
+        console.log("Random Number: " + item);
+
+
+main();
+
+    return data;
